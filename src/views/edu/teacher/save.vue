01dca0dd681dd99422f595d16798ecfdf65056d2
lab507_win <yfh97@qq.com>
2022-04-22 22:19:01 +0800

    阿里云对象存储oss上传头像，nginx请求转发（谷粒学院day6）

--- src/views/edu/teacher/save.vue
+++ src/views/edu/teacher/save.vue
@@ -8,7 +8,7 @@
         <el-input-number
           v-model="teacherInfo.sort"
           controls-position="right"
-          min="0"
+          :min="0"
         />
       </el-form-item>
       <el-form-item label="讲师头衔">
@@ -25,6 +25,35 @@
       </el-form-item>
 
       <!-- 讲师头像：TODO -->
+      <!-- 讲师头像 -->
+      <el-form-item label="讲师头像">
+
+          <!-- 头衔缩略图 -->
+          <pan-thumb :image="teacherInfo.avatar"/>
+          <!-- 文件上传按钮 -->
+          <el-button type="primary" icon="el-icon-upload" @click="imagecropperShow=true">更换头像
+          </el-button>
+
+          <!--
+      v-show：是否显示上传组件
+      :key：类似于id，如果一个页面多个图片上传控件，可以做区分
+      :url：后台上传的url地址
+      @close：关闭上传组件
+      @crop-upload-success：上传成功后的回调 
+        <input type="file" name="file"/>
+      -->
+          <image-cropper
+                        v-show="imagecropperShow"
+                        :width="300"
+                        :height="300"
+                        :key="imagecropperKey"
+                        :url="BASE_API+'/eduoss/fileoss'"
+                        field="multipartFile"
+                        @close="close"
+                        @crop-upload-success="cropSuccess"/>
+      </el-form-item>
+
+
 
       <el-form-item>
         <el-button
@@ -40,7 +69,13 @@
 
 <script>
 import teacher from "@/api/edu/teacher";
+// 引入组件
+import ImageCropper from "@/components/ImageCropper";
+import PanThumb from "@/components/PanThumb";
+
 export default {
+  components: { ImageCropper, PanThumb },
+
   data() {
     return {
       teacherInfo: {
@@ -52,11 +87,18 @@ export default {
         intro: "",
         avatar: "",
       },
+
+      //上传弹框组件是否显示
+      imagecropperShow: false,
+      imagecropperKey: 0, //上传组件key值
+      BASE_API: process.env.BASE_API, //获取dev.env.js里面地址
+      saveBtnDisabled: false, // 保存按钮是否禁用,
     };
   },
   created() {
+    //  console.log(this.BASE_API)
     // 在数据渲染之前，调用判断路径是否有id 从而判断是add 还是update 讲师信息
-    this.init()
+    this.init();
   },
 
   watch: {
@@ -68,6 +110,23 @@ export default {
   },
 
   methods: {
+    close() {
+      // 关闭上传头像 弹框
+      this.imagecropperShow = false;
+      // 每次点击close之后， 组件重新初始化， 因此把这个组件唯一标识号变换一个不一样的数
+      this.imagecropperKey = this.imagecropperKey + 1;
+    },
+
+    cropSuccess(data) {
+      // :url="BASE_API + '/eduoss/fileoss'" 从这里返回数据
+      // 上传成功 将返回的值 url复制给 avatar
+      this.imagecropperShow = false;
+      //上传之后接口返回图片地址
+      
+       this.teacherInfo.avatar = data.url;
+       this.imagecropperKey = this.imagecropperKey + 1;
+    },
+
     init() {
       // 方法抽取
       if (this.$route.params && this.$route.params.id) {
